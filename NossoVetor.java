dd0fca0e1445b1c7e640bbc0fdb9f42469608ff3
Lyssa Okawa Perini <lyoperini2@gmail.com>
2025-03-31 20:27:09 -0300

    Tentando fazer 10x direto cada teste

--- NossoVetor.java
+++ NossoVetor.java
@@ -6,29 +6,8 @@ public class NossoVetor {
     public long trocas;
     public long comparacoesBusca;
 
-    public NossoVetor(int tamanho) {
-        vetor = new int[tamanho];
-        gerarVetorAleatorio();
-    }
-
     public NossoVetor(int[] vetor) {
         this.vetor = vetor;
-    }
-
-    public int[] getVetor() {
-        return vetor;
-    }
-
-    public void setVetor(int[] vetor) {
-        this.vetor = vetor;
-    }
-
-    // Método para gerar vetor aleatório
-    private void gerarVetorAleatorio() {
-        Random rand = new Random();
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = rand.nextInt(1600000);
-        }
     }
 
     // Métodos de ordenação (Bubble Sort)
@@ -99,7 +78,6 @@ public class NossoVetor {
         for (int i = 0; i < vetor.length; i++) {
             comparacoesBusca++;
             if (vetor[i] == chave) {
-                
                 return i;
             }
         }
@@ -125,6 +103,6 @@ public class NossoVetor {
                 inicio = meio + 1; 
             } 
         }
-    return -1; 
+        return -1; 
     }
 }
